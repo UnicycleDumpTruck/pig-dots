7d80d6cf6748e3137f63d573b5c210c7aadc6fe5
Jeff <junk@jeffhighsmith.com>
2023-02-10 17:05:07 -0500

    WIP: trying to get serial working

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,61 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <Adafruit_SleepyDog.h>
 
-// Project Includes
+// Project Includes/dev/ttyUSB0
 #include "Version.h"
 #include <pinout.h>
-#include <audio.h>
-#include <radio.h>
 
-// Button Header
-// Include the Bounce2 library found here :
-// https://github.com/thomasfredericks/Bounce2
-#include <Bounce2.h>
+SoftwareSerial link(4,5);
+// char old_count_text = "";
+// char count_text = "";
+// how much serial data we expect before a newline
+const unsigned int MAX_INPUT = 50;
 
-// INSTANTIATE A Bounce OBJECT
-Bounce bounce = Bounce();
 
 // SET A VARIABLE TO STORE THE LED STATE
 int ledState = LOW;
+
+
+void process_data (const char * data)
+  {
+  // for now just display it
+  // (but you could compare it to some value, convert to an integer, etc.)
+  Serial.println (data);
+  }  // end of process_data
+  
+void processIncomingByte (const byte inByte)
+  {
+  static char input_line [MAX_INPUT];
+  static unsigned int input_pos = 0;
+
+  switch (inByte)
+    {
+
+    case '\n':   // end of text
+      input_line [input_pos] = 0;  // terminating null byte
+      
+      // terminator reached! process input_line here ...
+      process_data (input_line);
+      
+      // reset buffer for next time
+      input_pos = 0;  
+      break;
+
+    case '\r':   // discard carriage return
+      break;
+
+    default:
+      // keep adding if not full ... allow for terminating null byte
+      if (input_pos < (MAX_INPUT - 1))
+        input_line [input_pos++] = inByte;
+      break;
+
+    }  // end of switch
+   
+  } // end of processIncomingByte  
+
+
 
 // ███████╗███████╗████████╗██╗   ██╗██████╗
 // ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
@@ -27,33 +66,26 @@ int ledState = LOW;
 
 void setup()
 {
-  Serial.begin(9600);
-  while (!Serial)
-  {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.printf("\nProject version v%s, built %s\n", VERSION, BUILD_TIMESTAMP);
+  Serial.begin(115200);
+  // while (!Serial)
+  // {
+  //   ; // wait for serial port to connect. Needed for native USB port only
+  // }
+  //Serial.printf("\nProject version v%s, built %s\n", VERSION, BUILD_TIMESTAMP);
   Serial.println("Setup function commencing...");
-  vsAudioSetup();
-  delay(100);
-  radioSetup();
-
-  // BOUNCE SETUP
-
-  // SELECT ONE OF THE FOLLOWING :
-  // 1) IF YOUR INPUT HAS AN INTERNAL PULL-UP
-  bounce.attach(BOUNCE_PIN, INPUT_PULLUP); // USE INTERNAL PULL-UP
-  // 2) IF YOUR INPUT USES AN EXTERNAL PULL-UP
-  //bounce.attach( BOUNCE_PIN, INPUT ); // USE EXTERNAL PULL-UP
-
-  // DEBOUNCE INTERVAL IN MILLISECONDS
-  bounce.interval(5); // interval in ms
 
   // LED SETUP
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, ledState);
 
-  Watchdog.enable(4000);
+  // Watchdog.enable(4000);
+
+  link.begin(300);
+  delay(2000);
+  link.print('b');
+  delay(20);
+  link.print('b');
+
   Serial.println("Setup Complete");
 }
 
@@ -66,26 +98,21 @@ void setup()
 
 void loop()
 {
-  // Update the Bounce instance (YOU MUST DO THIS EVERY LOOP)
-  bounce.update();
+  while (link.available() == 0) {}
+  Serial.println("=========================================================================");
+  processIncomingByte (link.read ());
+  // byte count = link.read();
+  // // int count_int = link.parseInt();
+  // // Serial.println(count_int);
+  // // if(count > 0)
+  // // { 
+  // Serial.print("count received: ");
+  // Serial.println(count);
+  // ledState = !ledState;
+  // digitalWrite(LED_PIN, ledState);
+  // link.print(count);
+  // }
 
-  // <Bounce>.changed() RETURNS true IF THE STATE CHANGED (FROM HIGH TO LOW OR LOW TO HIGH)
-  if (bounce.changed())
-  {
-    // THE STATE OF THE INPUT CHANGED
-    // GET THE STATE
-    int deboucedInput = bounce.read();
-    // IF THE CHANGED VALUE IS LOW
-    if (deboucedInput == LOW)
-    {
-      ledState = !ledState;            // SET ledState TO THE OPPOSITE OF ledState
-      digitalWrite(LED_PIN, ledState); // WRITE THE NEW ledState
-      startAudio();
-      delay(1000);
-      stopAudio();
-      sendGoEvent(1); // Does not work inside VS1053 audio startPlayingFile!
-    }
-  }
 
-  Watchdog.reset();
+  // Watchdog.reset();
 }
